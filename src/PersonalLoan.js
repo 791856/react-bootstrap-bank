e2997514a10fcd0489811c9c2819f5a817ecba25
Subhnkar <Subhnkar.Das4@Cognizant.com>
2020-09-11 17:39:55 +0530

    Adding Validation in deposit and personal loan page

--- src/PersonalLoan.js
+++ src/PersonalLoan.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Card, Button, Row, Col, Form } from 'react-bootstrap';
 
-import { Formik } from 'formik';
+import { Formik, yupToFormErrors } from 'formik';
 import * as yup from 'yup'
 
 const schema = yup.object({
     Anualincome: yup.string()
         .required(),
     CompanyName: yup.string()
-    .required()
+    .required(),
+    Designation: yup.string()
+    .required(),
+    TotalExp:  yup.string()
+    .required(),
+    ExpwithCurrentCompany : yup.string()
+    .required(),
 
 });
 
@@ -24,7 +30,9 @@ export const PersonalLoan = () => {
 
                     Anualincome: '',
                     CompanyName: '',
-
+                    Designation: '',
+                    TotalExp:    '',
+                    ExpwithCurrentCompany: '',
                 }}
             >
                 {({
@@ -71,21 +79,55 @@ export const PersonalLoan = () => {
 
                             </Form.Group>
 
-                            <Form.Group controlId="Father Name">
+                            <Form.Group controlId="Designation">
                                 <Form.Label><strong>Designation </strong></Form.Label>
-                                <Form.Control type="RateofIntrest" placeholder="" />
+                                <Form.Control 
+                                
+                                type="number" 
+                                placeholder="Enter Account Designation"
+                               name="AccountNumber"
+                               value={values.Designation}
+                               onChange={handleChange}
+
+                           />
+                           {errors.Designation && touched.Designation? (
+                               <div>{errors.Designation}</div>
+                           ) : null}
                             </Form.Group>
 
-                            <Form.Group controlId="Total Exp">
+                            <Form.Group controlId="TotalExp">
                                 <Form.Label><strong>Total exp</strong></Form.Label>
-                                <Form.Control type="Total Exp" placeholder="" />
+                                <Form.Control 
+                                type="number" 
+                                placeholder="Enter Total Exp"
+                               name="AccountNumber"
+                               value={values.TotalExp}
+                               onChange={handleChange}
+
+                           />
+                           {errors.TotalExp && touched.TotalExp? (
+                               <div>{errors.TotalExp}</div>
+                           ) : null}
+                           
+                        
                             </Form.Group>
 
-                            <Form.Group controlId="Exp with Current company">
-                                <Form.Label><strong>Exp with Current Company </strong></Form.Label>
-                                <Form.Control type="Anual income" placeholder="" />
-                            </Form.Group>
+                            <Form.Group controlId="ExpwithCurrentCompany">
+                                <Form.Label><strong>ExpWithCurrentCompany </strong></Form.Label>
+                                <Form.Control 
+                                
+                                type="number" 
+                                placeholder="Enter Total Exp in Current Company"
+                               name="AccountNumber"
+                               value={values.ExpwithCurrentCompany}
+                               onChange={handleChange}
 
+                           />
+                           {errors.ExpwithCurrentCompany && touched.ExpwithCurrentCompany? (
+                               <div>{errors.ExpwithCurrentCompany}</div>
+                           ) : null}
+                            </Form.Group>
+                            
                             <Button variant="primary" type="submit">
                             {/* href="http://localhost:3000/sucess */}
                                 ApplyForPersonalLoan
